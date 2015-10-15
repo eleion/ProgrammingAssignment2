2a17847094049a76acdc5e92d4b62c2dbf8a308a
Luis B. <luis_ness@hotmail.com>
2015-10-15 13:23:50 +0200

    Ejercicios Tarea2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,53 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   # Inicializamos la matrix.
+   inversa <- NULL
+   
+   # Creamos las funciones 'set' y 'get'.
+   set <- function(y) {
+      x       <<- y
+      inversa <<- NULL
+   }
+   get <- function() {
+      x
+   }
+   # Creamos las funciones 'setInversa' y 'getInversa'.
+   setInversa <- function(i) {
+      inversa <<- i
+   }
+   getInversa <- function() {
+      inversa
+   }
+   
+   # Devolvemos una lista con los métodos 'gets' y 'sets'.
+   list(
+      set = set,
+      get = get,
+      setInversa = setInversa,
+      getInversa = getInversa
+   )
 }
 
 
 ## Write a short comment describing this function
+## Devuelve una matriz que es la inversa de 'x'.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   # Obtenemos la inversa.
+   inversa <- x$getInversa()
+   
+   # Si no es nula la inversa la calculamos.
+   if(!is.null(inversa)) {
+      message('Obteniendo la inversa de la matriz.')
+      return(inversa)
+   }
+   
+   # Obtenemos la matriz y calculamos la inversa.
+   matriz <- x$get()
+   inversa <- solve(matriz, ...)
+   x$setInversa(inversa)
+   
+   # Devolvemos la matriz inversa.
+   inversa
 }
